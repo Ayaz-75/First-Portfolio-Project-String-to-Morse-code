8e6fa92ad5c1baf2005a77a4b1bdf8833acff7ef
Ayaz Ali <ayazalilakho2@gmail.com>
2023-06-05 00:42:56 +0500

    First portfolio project

---
+++ strings_to_morse.py
@@ -0,0 +1,36 @@
+values = {
+    "a": "._",
+    "b": "_...",
+    "c": "_._.",
+    "d": "_..",
+    "e": ".",
+    "f": ".._.",
+    "g": "__.",
+    "h": "....",
+    "i": "..",
+    "j": ".___",
+    "k": "_._",
+    "l": "._..",
+    "m": "__",
+    "n": "_.",
+    "o": "___",
+    "p": ".__.",
+    "q": "__._",
+    "r": "._.",
+    "t": "...",
+    "u": ".._",
+    "v": "..._",
+    "w": ".__",
+    "x": "_.._",
+    "y": "_.__",
+    "z": "__..",
+}
+
+
+input_string = input("enter the string: ")
+for letter in input_string:
+    if letter in values:
+        print(values[letter])
+    else:
+        print(letter)
+
